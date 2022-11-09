cfcaddd8eb8bf5681ea2bec73b8f6ac5109c7362
Mateusz <nowe7246@gmail.com>
2022-11-09 17:15:24 +0100

    Created constntns width and colors. Created simple structure in app component.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,14 @@
 import './App.css';
 
-export const App = () => (
-    <div className="App">
-        <div>hello world</div>
-    </div>
-);
+export const App = () => {
+    return (
+        <div className="App">
+            <div className='game__container'>
+                <h1>Witaj w Paint Game</h1>
+                <div className='board'>Board</div>
+                <div className='buttons'>Buttons</div>
+            </div>
+        </div>
+    );
 
-;
+}
---
+++ src/constatns/index.js
@@ -0,0 +1,3 @@
+export const WIDTH = 14;
+
+export const COLORS = ['red', 'green', 'blue', 'yellow', 'white', 'orange'];
