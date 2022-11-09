9edf87fd4c672d12d7419b933c04637794e51572
Mateusz <nowe7246@gmail.com>
2022-11-09 20:16:04 +0100

    Changed starter colors.

--- src/constatns/index.js
+++ src/constatns/index.js
@@ -2,4 +2,4 @@ export const WIDTH = 14;
 
 export const MOVES= 35;
 
-export const COLORS = ['red', 'green', 'blue', 'yellow', 'white', 'orange'];
+export const COLORS = ['#ffa9f3', '#0889ff', '#0d236e', '#fff854', '#0c7219', '#c85040'];
