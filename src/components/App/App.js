bd23fddcef768c07ddb2ae3814e90cbcebed08a0
Mateusz <nowe7246@gmail.com>
2022-11-10 08:35:06 +0100

    Created buttons component and move select color logic there.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,8 @@
 import {useState} from "react";
-import {createBoard} from "../../utils/generateBoard";
 import {Board} from "../Board/Board";
-import {COLORS, MOVES, WIDTH} from "../../constatns";
+import {Buttons} from "../Buttons/Buttons";
+import {createBoard} from "../../utils/generateBoard";
+import {MOVES} from "../../constatns";
 import './App.css';
 
 export const App = () => {
@@ -9,51 +10,6 @@ export const App = () => {
     const [moves, setMoves] = useState(MOVES);
     const [isWonGame, setIsWonGame] = useState(false);
     const [numberOfWonGames, setNumberOfWonGames] = useState(1);
-
-    const handleColorClick = (color) => {
-        cellsArray[0].active = true;
-        cellsArray[0].value = color;
-
-        if (moves !== MOVES) {
-            for (let i = 0; i < WIDTH * WIDTH; i++) {
-                if (cellsArray[i].active === true) {
-                    cellsArray[i].value = color;
-                }
-            }
-        }
-        const activeCellIndexArray = [];
-        activeCellIndexArray.push(cellsArray[0].index);
-
-        const foundedColorsArray = cellsArray.map(square => {
-            if ((activeCellIndexArray.includes(square.index - 1)
-                    && square.value === color)
-                ||
-                (activeCellIndexArray.includes(square.index - WIDTH)
-                    && square.value === color)
-                ||
-                (activeCellIndexArray.includes(square.index + 1)
-                    && square.value === color)
-            ) {
-                square.active = true
-                activeCellIndexArray.push(square.index)
-            }
-            return square
-        })
-
-        if (activeCellIndexArray.length === cellsArray.length) {
-            setIsWonGame(true);
-        }
-
-        setCellsArray(foundedColorsArray)
-        setMoves(moves - 1)
-    };
-
-    const restartGame = () => {
-        setCellsArray(createBoard());
-        setMoves(MOVES);
-        setNumberOfWonGames(1)
-        setIsWonGame(false);
-    };
 
     return (
         <div className="App">
@@ -71,26 +27,14 @@ export const App = () => {
                         setMoves={setMoves}
                         setNumberOfWonGames={setNumberOfWonGames}
                     />
-                    <div className='buttons__container'>
-                        <h3>Wybierz kolor:</h3>
-                        <div>
-                            {COLORS.map((color, index) => (
-                                <button
-                                    key={index}
-                                    style={{backgroundColor: color}}
-                                    className='button'
-                                    disabled={moves === 0}
-                                    onClick={() => handleColorClick(color)}
-                                />
-                            ))}
-                        </div>
-                        <button
-                            className='btn'
-                            onClick={restartGame}
-                        >
-                            Restart
-                        </button>
-                    </div>
+                    <Buttons
+                        setNumberOfWonGames={setNumberOfWonGames}
+                        setMoves={setMoves}
+                        moves={moves}
+                        setCellsArray={setCellsArray}
+                        cellsArray={cellsArray}
+                        setIsWonGame={setIsWonGame}
+                    />
                 </div>
             </div>
         </div>
---
+++ src/components/Buttons/Buttons.js
@@ -0,0 +1,73 @@
+import {createBoard} from "../../utils/generateBoard";
+import {COLORS, MOVES, WIDTH} from "../../constatns";
+import './Buttons.css';
+
+export const Buttons = ({cellsArray, moves, setIsWonGame, setCellsArray, setMoves, setNumberOfWonGames}) => {
+    const handleColorClick = (color) => {
+        cellsArray[0].active = true;
+        cellsArray[0].value = color;
+
+        if (moves !== MOVES) {
+            for (let i = 0; i < WIDTH * WIDTH; i++) {
+                if (cellsArray[i].active === true) {
+                    cellsArray[i].value = color;
+                }
+            }
+        }
+        const activeCellIndexArray = [];
+        activeCellIndexArray.push(cellsArray[0].index);
+
+        const foundedColorsArray = cellsArray.map(square => {
+            if ((activeCellIndexArray.includes(square.index - 1)
+                    && square.value === color)
+                ||
+                (activeCellIndexArray.includes(square.index - WIDTH)
+                    && square.value === color)
+                ||
+                (activeCellIndexArray.includes(square.index + 1)
+                    && square.value === color)
+            ) {
+                square.active = true
+                activeCellIndexArray.push(square.index)
+            }
+            return square
+        })
+
+        if (activeCellIndexArray.length === cellsArray.length) {
+            setIsWonGame(true);
+        }
+
+        setCellsArray(foundedColorsArray)
+        setMoves(moves - 1)
+    };
+
+
+    const restartGame = () => {
+        setCellsArray(createBoard());
+        setMoves(MOVES);
+        setNumberOfWonGames(1)
+        setIsWonGame(false);
+    };
+    return (
+        <div className='buttons__container'>
+            <h3>Wybierz kolor:</h3>
+            <div>
+                {COLORS.map((color, index) => (
+                    <button
+                        key={index}
+                        style={{backgroundColor: color}}
+                        className='button'
+                        disabled={moves === 0}
+                        onClick={() => handleColorClick(color)}
+                    />
+                ))}
+            </div>
+            <button
+                className='btn'
+                onClick={restartGame}
+            >
+                Restart
+            </button>
+        </div>
+    )
+}
